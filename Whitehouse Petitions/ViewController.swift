08034dd89fdcc6ba0fec00e5be9f1eacafd3bcf1
Promal <promal.barua.cse@gmail.com>
2021-10-23 23:05:46 +0600

    json + async

--- Whitehouse Petitions/ViewController.swift
+++ Whitehouse Petitions/ViewController.swift
@@ -20,21 +20,26 @@ class ViewController: UITableViewController {
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
 
-
-        if let url = URL(string: urlString) {
-            if let data = try? Data(contentsOf: url) {
-                parseJson(json: data)
-                return
+        DispatchQueue.global(qos: .userInitiated).async {
+            [weak self] in
+            if let url = URL(string: urlString) {
+                if let data = try? Data(contentsOf: url) {
+                    self?.parseJson(json: data)
+                    return
+                }
             }
+            self?.showError()
         }
+        
 
-        showError()
     }
     
     func showError() {
-        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
+        DispatchQueue.main.async { [weak self] in
+            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self?.present(ac, animated: true)
+        }
     }
     
     func parseJson(json: Data){
@@ -42,7 +47,9 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json){
             petitions = jsonPetitions.results
-            tableView.reloadData()
+            DispatchQueue.main.async { [weak self] in
+                self?.tableView.reloadData()
+            }
         }
     }
     
